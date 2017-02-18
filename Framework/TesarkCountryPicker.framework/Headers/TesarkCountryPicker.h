b6b4d5ea879154636aee71651ffe0e5135b85630
Kalidoss Shanmugam <kalidoss@tesark.in>
2017-02-18 14:41:20 +0530

    Framework Update

--- Framework/TesarkCountryPicker.framework/Headers/TesarkCountryPicker.h
+++ Framework/TesarkCountryPicker.framework/Headers/TesarkCountryPicker.h
@@ -12,6 +12,8 @@ FOUNDATION_EXPORT double TesarkCountryPickerVersionNumber;
 
 //! Project version string for TesarkCountryPicker.
 FOUNDATION_EXPORT const unsigned char TesarkCountryPickerVersionString[];
+#import "TesarkCountryPicker-Swift.h"
+
 
 // In this header, you should import all the public headers of your framework using statements like
 
